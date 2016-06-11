63cd368705f5e42295be2bb8526689ad30e8d293
Roaming <rangerroaming@gmail.com>
2016-06-11 07:52:32 +0530

    Adding some timer functions

--- examples/i2c_mpsse/timer_lib.c
+++ examples/i2c_mpsse/timer_lib.c
@@ -14,6 +14,11 @@
 
 #define MAX_TIMERS 5
 
+#define CKCON_T0 0x08
+#define CKCON_T1 0x10
+#define CKCON_T2 0x20
+#define T0_MODE_REL 0x02
+
 struct timer
 {
     unsigned short expiry;
@@ -30,5 +35,19 @@ __xdata static volatile unsigned short fx2_tick = 0;
 void timerlib_init()
 {
 
+    CKCON |= CKCON_T0;
+    TMOD  |= T0_MODE_REL;
 
+}
+
+
+void timer_interrupt_enable()
+{
+    ENABLE_TIMER0();
+
+}
+
+void timer_start()
+{
+    TR0 = 1;
 }
